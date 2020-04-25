193fab070a5cec40cf85c0eeccdcda70348b385e
takahiro <>
2020-04-25 18:02:54 +0900

    CRUD機能を実装

--- apps/note/urls.py
+++ apps/note/urls.py
@@ -3,4 +3,8 @@ from .                import views
 
 urlpatterns=[
     path('',views.index,name='index'),
+    path('<int:note_id>',views.detail,name='detail'),
+    path('edit/<int:note_id>',views.edit),
+    path('delete/<int:note_id>',views.delete),
+    path('create/',views.create),
 ]
--- apps/note/views.py
+++ apps/note/views.py
@@ -1,7 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http      import HttpResponse
+from . models         import Note
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hellow')
+    notes=Note.objects.all()
+    context={
+        'notes':notes,
+    }
+    return render(request,'note/note_list.html',context)
+
+def detail(request,note_id):
+    try:
+        note=Note.objects.get(id=note_id)
+    except Note.DeosNoteExist:
+        note=None
+    
+    context={
+        'note':note
+    }
+    return render(request,'note/note_detail.html',context)
+
+def create(request):
+    if request.method=='POST':
+        note=Note()
+        note.title=request.POST['title']
+        note.content=request.POST['content']
+        note.save()
+        return index(request)
+    elif request.method=='GET':
+        return render(request,'note/note_create.html')
+
+
+def edit(request,note_id):
+    if request.method == 'POST':
+        note=Note.objects.get(id=note_id)
+        note.title=request.POST['title']
+        note.content=request.POST['content']
+        note.save()
+        return index(request)
+    elif request.method == 'GET':
+        note=Note.objects.get(id=note_id)
+        context={
+            'note':note
+        }
+        return render(request,'note/note_edit.html',context)
+
+def delete(request,note_id):
+     note=Note.objects.filter(id=note_id).delete()
+     return index(request)
